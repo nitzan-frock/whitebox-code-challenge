7c50e382788c33bfcb42c442a77eba49cd0086b4
Nitzan Frock <nitzanfrock@gmail.com>
2019-02-24 17:53:04 -0500

    begin wiring of SPA

---
+++ client/js/controllers/controllers.js
@@ -0,0 +1,15 @@
+'use strict';
+
+$('document').ready(function () {
+
+    // Sorting variables
+    const sortCategory = $('.select2-selection__rendered').attr('title');
+    console.log(sortCategory);
+    const sortPriceRange = $('');
+
+    // Filter variables
+    const price = null;
+    const color = null;
+
+
+});
