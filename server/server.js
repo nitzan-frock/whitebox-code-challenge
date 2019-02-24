7b541dd2676c597d4dd8a2ca11251af853bd55df
Nitzan Frock <nitzanfrock@gmail.com>
2019-02-24 16:24:28 -0500

    getMany and getSingle routes

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = 8080;
+
+const url = `https://next.json-generator.com/api/json/get/EkzBIUWNL`;
+
+const routes = require('./app/routes')(app, url);
+
+app.listen(PORT, () => {
+    console.log(`Listening on ${PORT}.`);
+});
